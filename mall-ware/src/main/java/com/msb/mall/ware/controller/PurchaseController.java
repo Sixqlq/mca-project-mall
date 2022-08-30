c9ac5107a48683e47443aa30135a9b9ffc2f1ec4
Sixqlq <2390608028@qq.com>
2022-08-31 02:28:29 +0800

    业务开发-库存管理

--- mall-ware/src/main/java/com/msb/mall/ware/controller/PurchaseController.java
+++ mall-ware/src/main/java/com/msb/mall/ware/controller/PurchaseController.java
@@ -1,15 +1,14 @@
 package com.msb.mall.ware.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 
+import com.msb.mall.ware.vo.MergeVO;
+import com.msb.mall.ware.vo.PurchaseDoneVO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.msb.mall.ware.entity.PurchaseEntity;
 import com.msb.mall.ware.service.PurchaseService;
@@ -30,6 +29,41 @@ import com.msb.common.utils.R;
 public class PurchaseController {
     @Autowired
     private PurchaseService purchaseService;
+
+    @RequestMapping("/done")
+    public R done(@RequestBody PurchaseDoneVO purchaseDoneVO){
+        purchaseService.done(purchaseDoneVO);
+        return R.ok();
+    }
+
+
+    /**
+     * 领取采购单
+     * [2,3,4]
+     * @return
+     */
+    @PostMapping("/receive")
+    public R receive(@RequestBody List<Long> ids){
+        purchaseService.received(ids);
+        return R.ok();
+    }
+
+    @RequestMapping("/merge")
+    public R merge(@RequestBody MergeVO mergeVO){
+        Integer flag = purchaseService.merge(mergeVO);
+
+        return R.ok();
+    }
+
+    /**
+     * 合并采购需求
+     */
+    @RequestMapping("/unreceive/list")
+    public R listUnreceive(@RequestParam Map<String, Object> params){
+        PageUtils page = purchaseService.queryPageUnreceive(params);
+
+        return R.ok().put("page", page);
+    }
 
     /**
      * 列表
--- mall-ware/src/main/java/com/msb/mall/ware/service/impl/WareSkuServiceImpl.java
+++ mall-ware/src/main/java/com/msb/mall/ware/service/impl/WareSkuServiceImpl.java
@@ -1,6 +1,11 @@
 package com.msb.mall.ware.service.impl;
 
+import com.msb.common.utils.R;
+import com.msb.mall.ware.feign.ProductFeignService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -11,19 +16,71 @@ import com.msb.common.utils.Query;
 import com.msb.mall.ware.dao.WareSkuDao;
 import com.msb.mall.ware.entity.WareSkuEntity;
 import com.msb.mall.ware.service.WareSkuService;
+import org.springframework.util.StringUtils;
 
 
 @Service("wareSkuService")
 public class WareSkuServiceImpl extends ServiceImpl<WareSkuDao, WareSkuEntity> implements WareSkuService {
 
+    @Autowired
+    private WareSkuDao skuDao;
+
+    private ProductFeignService productFeignService;
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<WareSkuEntity> wrapper = new QueryWrapper<>();
+        String skuId = (String) params.get("skuId");
+        if(!StringUtils.isEmpty(skuId)){
+            wrapper.eq("sku_id", skuId);
+        }
+
+        String wareId = (String) params.get("wareId");
+        if(!StringUtils.isEmpty(wareId)){
+            wrapper.eq("ware_id", wareId);
+        }
+
         IPage<WareSkuEntity> page = this.page(
                 new Query<WareSkuEntity>().getPage(params),
-                new QueryWrapper<WareSkuEntity>()
+                wrapper
         );
 
         return new PageUtils(page);
+    }
+
+    /**
+     * 入库操作
+     * @param skuId 商品编号
+     * @param wareId 仓库编号
+     * @param skuNum 采购商品的数量
+     */
+    @Override
+    public void addStock(Long skuId, Long wareId, Integer skuNum) {
+        // 判断是否有改商品和仓库的入库记录
+        List<WareSkuEntity> list = skuDao.selectList(new QueryWrapper<WareSkuEntity>().eq("sku_id", skuId).eq("ware_id", wareId));
+        if(list == null || list.size() == 0){
+            // 如果没有就新增商品库存记录
+            WareSkuEntity entity = new WareSkuEntity();
+            entity.setSkuId(skuId);
+            entity.setWareId(wareId);
+            entity.setStock(skuNum);
+            entity.setStockLocked(0);
+            try {
+                // 动态的设置商品的名称
+                R info = productFeignService.info(skuId); // 通过Feign远程调用商品服务的接口
+                Map<String,Object> data = (Map<String, Object>) info.get("skuInfo");
+                if(info.getCode() == 0){
+                    entity.setSkuName((String) data.get("skuName"));
+                }
+            }catch (Exception e){
+
+            }
+            skuDao.insert(entity); // 插入商品库存记录
+        }else{
+            // 如果有就更新库存
+            skuDao.addStock(skuId,wareId,skuNum);
+        }
+
     }
 
 }
